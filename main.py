3cf7d989c3110fad3363cd39159defccfec5f298
Meli00Cka <meli07cka@gmail.com>
2024-07-25 00:22:15 +0330

    Added Sidebar and Dataset preview

--- main.py
+++ main.py
@@ -4,44 +4,38 @@ import pandas as pd
 
 
 st.set_page_config(
-    initial_sidebar_state="expanded",menu_items=None,
+    initial_sidebar_state="collapsed",menu_items=None,
     page_icon="./img/taxi_logo.png",layout="wide",
     page_title="NY Taxi trips"
 )
 fu.local_css("style.css")
 data = fu.load_data()
-
+data_taxi_zones = fu.load_data("data/taxi-zone-lookup.csv")
 
 with st.sidebar:
+    st.markdown("### Taxi zones lookup")
+    selected_zone = st.slider(label="pick a zone",label_visibility="hidden",min_value=min(data_taxi_zones["LocationID"]),max_value=max(data_taxi_zones["LocationID"]))
+    selected_zone_data = data_taxi_zones.iloc[selected_zone,:]
+    st.write(f'Borough: {selected_zone_data["Borough"]}\n\nZone: {selected_zone_data["Zone"]}')
+    
     st.markdown("----")
     st.markdown("### Main Controls")
-    show_dataset = st.toggle("Dataset details", value=True)
-    show_1 = st.toggle("show_1", value=True)
-    show_2 = st.toggle("show_2", value=True)
-    show_3 = st.toggle("show_3", value=True)
+    show_dataset = st.toggle("Dataset", value=True)
+    show_analysis= st.toggle("Analyse", value=True)
+    show_predict = st.toggle("Predict", value=True)
     st.markdown("----")
     
-    st.markdown("### Plots")
-    st.markdown("##### First plot")
-    show_4 = st.toggle("show_4", value=True)
-    show_5 = st.toggle("show_5", value=True)
-    st.markdown("##### second plot")
-    show_6 = st.toggle("show_6", value=True)
-    show_7 = st.toggle("show_7", value=True)
-    st.markdown("----")
 
 
-with st.container():
-    st.markdown("# NY Taxi trips")
+st.title("NY Taxi trips")
 
-
-if show_dataset:
-    with st.container(border=True):
-        col3, col4 = st.columns([5,1.3])
-        selected_col = col3.multiselect(label="select",label_visibility="hidden",options=data.columns,default=["passenger_count","trip_distance","fare_amount","tip_amount","total_amount","payment_type","PULocationID","tolls_amount"])
-        col4.write("<br>",unsafe_allow_html=True)
-        col4.link_button("Dataset Source","https://www.kaggle.com/datasets/anandshaw2001/taxi-dataset",use_container_width=True,help="For more details")
-        
-        col1, col2 = st.columns(2)
-        col1.write(data.describe())
-        col2.write(data[selected_col].head(8))
+st.header("Dataset")
+with st.expander(label="",expanded=show_dataset):
+    col3, col4 = st.columns([5,1.3])
+    selected_col = col3.multiselect(label="select",label_visibility="hidden",options=data.columns,default=list(data.columns))
+    with col4:
+        st.write("<br>",unsafe_allow_html=True)
+        st.link_button("Dataset columns dictionary","https://www.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_yellow.pdf",use_container_width=True)
+        st.link_button("Dataset Source","https://www.kaggle.com/datasets/anandshaw2001/taxi-dataset",use_container_width=True,help="For more details")
+    
+    st.write(data[selected_col].head(8))
